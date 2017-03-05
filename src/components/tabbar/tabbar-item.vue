d1b47acda5621c49988ad8f35dd5794261d700c6
zjhr <604068311@qq.com>
2017-03-05 08:44:11 +0800

    修复事件问题

--- src/components/tabbar/tabbar-item.vue
+++ src/components/tabbar/tabbar-item.vue
@@ -1,5 +1,5 @@
 <template>
-    <span class="tabbar-item" :class="{'active': activeitem}" @click="tabbarItemClickFun">
+  <span class="tabbar-item" :class="{'active': activeitem}" @click="tabbarItemClickFun">
         <icon class="icon" v-if="icon" :name="icon" :href="href">
             <badge v-if="badge" slot="badge" :amStyle="badgeStyle" rounded>
                 {{badge}}
@@ -12,55 +12,56 @@
 </template>
 
 <script>
-    import Icon from '../icon';
-    import Badge from '../badge';
-    export default {
-        name: 'tabbar-item',
-        props: {
-            title: String, //文字
-            icon: String, //图标名称
-            href: { //是否加链接
-                type: [String, Boolean],
-                default: false
-            },
-            active: Boolean, //是否选中
-            badge: { //子项小徽章上显示的文字
-                type: [Boolean, String, Number],
-                default: false
-            },
-            badgeStyle: {
-                type: String,
-                default: "red"
-            } //小徽章颜色样式，默认为 alert
-        },
-        beforeMount() {
-            if (this.active) {
-                this.activeitem = this.active
-            }
-        },
-        data() {
-            return {
-                activeitem: false
-            }
-        },
-        methods: {
-            tabbarItemClickFun(e) {
-                if (this.$parent.noActive) return;
-                this.$parent.itemsIndex = this.currentIndex;
-                this.$nextTick(() => {
-                    this.$emit('tabbarItemClick', e)
-                })
-            }
-        },
-        components: {
-            Badge,
-            Icon
-        }
-
+  import Icon from '../icon';
+  import Badge from '../badge';
+  export default {
+    name: 'tabbar-item',
+    props: {
+      title: String, //文字
+      icon: String, //图标名称
+      href: { //是否加链接
+        type: [String, Boolean],
+        default: false
+      },
+      active: Boolean, //是否选中
+      badge: { //子项小徽章上显示的文字
+        type: [Boolean, String, Number],
+        default: false
+      },
+      badgeStyle: {
+        type: String,
+        default: "red"
+      } //小徽章颜色样式，默认为 alert
+    },
+    beforeMount() {
+      if (this.active) {
+        this.activeitem = this.active
+      }
+    },
+    data() {
+      return {
+        activeitem: false
+      }
+    },
+    methods: {
+      tabbarItemClickFun(e) {
+        this.$nextTick(() => {
+          this.$emit('tabbarItemClick', e)
+        })
+        if (this.$parent.noActive) return;
+        this.$parent.itemsIndex = this.currentIndex;
+      }
+    },
+    components: {
+      Badge,
+      Icon
     }
+
+  }
 
 </script>
 
 <style scoped>
+
 
 </style>
